89b629804dc52aed003086f199492c378c5ef1a7
Ibrohim <ibo.bahritidinov@gmail.com>
2026-02-22 22:27:56 +0500

    fix build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 
 set -o errexit
 
-pip install --upgrade pip
-pip install -r requirements.txt
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
 
 # Create necessary directories
 mkdir -p media/images media/audio media/videos media/documents logs
